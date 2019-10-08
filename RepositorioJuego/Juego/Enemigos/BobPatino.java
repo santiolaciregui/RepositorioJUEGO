249417e9df0cb0226bfb1090d30c91ec2ee7793b
agustinmayer99 <agustinmayer99@hotmail.com>
2019-10-08 16:49:46 -0300

    enemigos

--- RepositorioJuego/Juego/Enemigos/BobPatino.java
+++ RepositorioJuego/Juego/Enemigos/BobPatino.java
@@ -7,7 +7,22 @@ import Clases.Enemigo;
 public class BobPatino extends Enemigo {
 
 	public BobPatino(int x, int y) {
-		super(x,y);
+		super(x, y);
+		label.setIcon(new ImageIcon(getClass().getResource("/Imagenes/BobPatinoParado.png")));
+		label.setBounds(x, y,117 , 110);
+	}
+	
+	public void atacarGrafico() {
+		label.setIcon(new ImageIcon(getClass().getResource("/Imagenes/BobPatinoAtacaGif.gif")));
+	}
+	
+	public void pararGrafico() {
+		label.setIcon(new ImageIcon(getClass().getResource("/Imagenes/BobPatinoParado.png")));
 	}
 
+	@Override
+	public Enemigo clonar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- RepositorioJuego/Juego/Enemigos/Burns.java
+++ RepositorioJuego/Juego/Enemigos/Burns.java
@@ -7,8 +7,23 @@ import Clases.Enemigo;
 public class Burns extends Enemigo {
 
 	public Burns(int x, int y) {
-		super(x,y);
-	
+		super(x, y);
+		label.setIcon(new ImageIcon(getClass().getResource("/Imagenes/BurnsParado.png")));
+		label.setBounds(x, y,117 , 110);
 	}
+	
+	public void atacarGrafico() {
+		label.setIcon(new ImageIcon(getClass().getResource("/Imagenes/BurnsAtacaGif.gif")));
+	}
+	
+	public void pararGrafico() {
+		label.setIcon(new ImageIcon(getClass().getResource("/Imagenes/BurnsParado.png")));
+	
+}
 
+	@Override
+	public Enemigo clonar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
