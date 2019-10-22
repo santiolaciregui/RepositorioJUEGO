67709d03633bccc654c65ccff485a918151793cf
agustinmayer99 <agustinmayer99@hotmail.com>
2019-10-22 09:25:35 -0300

    Testeo de patron Singleton
    package ajeno al proyecto.

---
+++ RepositorioJuego/Juego/testeoDePatronesAgus/Singleton.java
@@ -0,0 +1,9 @@
+package testeoDePatronesAgus;
+
+public final class Singleton {
+	private static final Singleton INSTANCE = new Singleton();
+	private Singleton() {}
+	public static Singleton getInstance() {
+		return INSTANCE;
+	}
+}
