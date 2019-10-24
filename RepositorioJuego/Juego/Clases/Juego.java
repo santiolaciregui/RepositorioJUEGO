3d41d1bc4f923886d7940786fbdb49d218bfaeac
agustinmayer99 <agustinmayer99@hotmail.com>
2019-10-24 14:41:30 -0300

    1ww

--- RepositorioJuego/Juego/Clases/Juego.java
+++ RepositorioJuego/Juego/Clases/Juego.java
@@ -20,7 +20,7 @@ public class Juego {
 		entidades= new LinkedList<GameObject>();
 		entidadesAeliminar= new LinkedList<GameObject>();
 		entidadesPendientes= new LinkedList<GameObject>();
-		this.mapa=new Mapa2(this);
+		this.mapa=new Mapa1(this);
 		tienda= new Tienda(this);
 		monedas=10000000;	
 		puntaje=0;
